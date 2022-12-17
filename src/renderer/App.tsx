ac91fee2e76d4436f9ddf9a3452d594165a7b90d
lojith vinsuka <lojithvinsuka1998@gmail.com>
2022-12-17 08:12:11 +0530

    test run

---
+++ src/renderer/App.tsx
@@ -0,0 +1,21 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import icon from '../../assets/icon.svg';
+import './App.css';
+
+const Hello = () => {
+  return (
+    <div>
+     Hello
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  );
+}
